537de524956baf767b5020fde43b424f48b50b16
Md Abu Naser Kayes <abunaserkayes0@gmail.com>
2022-04-30 03:51:34 +0600

    fixed liked section

--- js/app.js
+++ js/app.js
@@ -153,6 +153,7 @@ const showPosts = (posts) => {
 
 const displayLikedPosts = () => {
   const likedPosts = getLikedPosts();
+  document.getElementById("liked").textContent = "";
   likedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("liked").appendChild(div);
@@ -161,7 +162,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
-  document.getElementById("reported").innerHTML='';
+  document.getElementById("reported").innerHTML = "";
   reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
